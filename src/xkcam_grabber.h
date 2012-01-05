2a145a549caed155ca91aed4c6cf79287a7c5928
David Gibson <gibson@cs.bris.ac.uk>
2012-01-05 21:44:20 +0000

    first commit

--- src/xkcam_grabber.h
+++ src/xkcam_grabber.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 #ifndef KCAM_GRABBER_H_
 #define KCAM_GRABBER_H_
 
